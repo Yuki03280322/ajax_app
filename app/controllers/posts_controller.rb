d610a8a67a7b7a0963331287fa1f2eac50c66093
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-10-28 21:42:46 +0900

    AfaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all  # すべてのレコードを@postsに代入
-  end
-  
-  def new
+    @posts = Post.all.order(id: "DESC")  # すべてのレコードを@postsに代入
   end
 
   def create #contentというカラムにparams[:content]の情報を保存
     Post.create(content: params[:content]) #モデル.create(カラム名: 値)
+    redirect_to action: :index
   end #paramsという箱の中に格納されて送られてきたデータ(:content)
 end
